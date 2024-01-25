d5bb441ad67e25fc9fdce917b84ca415daa756ae
Tenzin <tenzintsunduebhattu@gmail.com>
2024-01-25 15:05:19 +0530

    write_tokenized_text_to_file

--- src/mt_aligner_prep_tool/config.py
+++ src/mt_aligner_prep_tool/config.py
@@ -11,3 +11,6 @@ def _mkdir(path):
 BASE_PATH = _mkdir(Path.home() / ".mt_files")
 BO_FILES_PATH = _mkdir(BASE_PATH / "tibetan_files")
 EN_FILES_PATH = _mkdir(BASE_PATH / "english_files")
+
+"""Path to the folder where the tokenized files(both english and tibetan files) will be stored"""
+TOKENIZED_FILES_PATH = _mkdir(BASE_PATH / "tokenized_files")
--- src/mt_aligner_prep_tool/pipeline.py
+++ src/mt_aligner_prep_tool/pipeline.py
@@ -1,6 +1,10 @@
 from typing import List
 
-from mt_aligner_prep_tool.config import BO_FILES_PATH, EN_FILES_PATH
+from mt_aligner_prep_tool.config import (
+    BO_FILES_PATH,
+    EN_FILES_PATH,
+    TOKENIZED_FILES_PATH,
+)
 from mt_aligner_prep_tool.download import clone_github_repo, find_first_txt_file
 from mt_aligner_prep_tool.tokenizers import sent_tokenize
 
@@ -25,8 +29,21 @@ def download_tibetan_english_files(ids: List[str]):
             tokenized_english_text = sent_tokenize(
                 english_txt_file.read_text(), lang="en"
             )
-            print(tokenized_tibetan_text)
-            print(tokenized_english_text)
+
+            # Write both tokenized text to files in TOKENIZED_FILES_PATH
+            with open(
+                TOKENIZED_FILES_PATH / f"tokenized_{tibetan_id}.txt",
+                "w",
+                encoding="utf-8",
+            ) as f:
+                f.write(tokenized_tibetan_text)
+
+            with open(
+                TOKENIZED_FILES_PATH / f"tokenized_{english_id}.txt",
+                "w",
+                encoding="utf-8",
+            ) as f:
+                f.write(tokenized_english_text)
 
 
 if __name__ == "__main__":
