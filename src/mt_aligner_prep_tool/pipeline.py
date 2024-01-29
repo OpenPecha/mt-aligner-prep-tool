0f79f603e72dfd2134b400ade6f1ecc1d68c1228
Tenzin <tenzintsunduebhattu@gmail.com>
2024-01-29 10:52:17 +0530

    upload_file_to_s3

--- src/mt_aligner_prep_tool/pipeline.py
+++ src/mt_aligner_prep_tool/pipeline.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 from typing import List
+
+import boto3
 
 from mt_aligner_prep_tool.config import (
     BO_FILES_PATH,
@@ -7,6 +10,14 @@ from mt_aligner_prep_tool.config import (
 )
 from mt_aligner_prep_tool.download import clone_github_repo, find_first_txt_file
 from mt_aligner_prep_tool.tokenizers import sent_tokenize
+
+
+def upload_file_to_s3(local_file_path: Path, bucket: str, s3_file: str):
+    """local_file_path: Path to the file to upload"""
+    """bucket: Bucket to upload to"""
+    """s3_file: file name to be upload to s3, folder path in s3 bucket is included in the file name"""
+    s3_client = boto3.client("s3")
+    s3_client.upload_file(local_file_path, bucket, s3_file)
 
 
 def download_tibetan_english_files(ids: List[str]):
@@ -28,12 +39,28 @@ def download_tibetan_english_files(ids: List[str]):
         tokenized_tibetan_text = sent_tokenize(tibetan_txt_file.read_text(), lang="bo")
         tokenized_english_text = sent_tokenize(english_txt_file.read_text(), lang="en")
 
-        # Write both tokenized texts to files in TOKENIZED_FILES_PATH
-        (TOKENIZED_FILES_PATH / f"tokenized_{tibetan_id}.txt").write_text(
-            tokenized_tibetan_text
+        """Write both tokenized texts to files in TOKENIZED_FILES_PATH"""
+        tokenized_tibetan_file_path = (
+            TOKENIZED_FILES_PATH / f"tokenized_{tibetan_id}.txt"
         )
-        (TOKENIZED_FILES_PATH / f"tokenized_{english_id}.txt").write_text(
-            tokenized_english_text
+        tokenized_english_file_path = (
+            TOKENIZED_FILES_PATH / f"tokenized_{english_id}.txt"
+        )
+
+        tokenized_english_file_path.write_text(tokenized_tibetan_text)
+
+        tokenized_english_file_path.write_text(tokenized_english_text)
+
+        """Upload both tokenized texts to S3"""
+        upload_file_to_s3(
+            local_file_path=tokenized_tibetan_file_path,
+            bucket="monlam.ai.tms",
+            s3_file=f"tokenized_bo/{tokenized_tibetan_file_path.name}",
+        )
+        upload_file_to_s3(
+            local_file_path=tokenized_english_file_path,
+            bucket="monlam.ai.tms",
+            s3_file=f"tokenized_en/{tokenized_english_file_path.name}",
         )
 
 
