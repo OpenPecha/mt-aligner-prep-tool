4a1b5bd5ddf755511da7567de723cc4105f38288
Tenzin <tenzintsunduebhattu@gmail.com>
2024-04-04 16:00:53 +0530

    merge multiple branches to main with args

--- src/mt_aligner_prep_tool/merge_branch.py
+++ src/mt_aligner_prep_tool/merge_branch.py
@@ -1,7 +1,13 @@
+import argparse
+import os
+from pathlib import Path
+
 import requests
 
+from mt_aligner_prep_tool.pipeline import get_file_content_by_lines
 
-def merge_branch_to_main(org_name, repo_name, branch_name, token):
+
+def merge_branch_to_main(repo_name: str, branch_name: str, org_name: str):
     """
     Merge a specified branch into the main branch of a given GitHub repository.
 
@@ -11,6 +17,9 @@ def merge_branch_to_main(org_name, repo_name, branch_name, token):
     - branch_name: Name of the branch to merge into main.
     - token: GitHub personal access token with repo permissions.
     """
+
+    token = os.environ.get("GITHUB_TOKEN")
+
     api_url = f"https://api.github.com/repos/{org_name}/{repo_name}/merges"
     headers = {
         "Authorization": f"token {token}",
@@ -29,3 +38,33 @@ def merge_branch_to_main(org_name, repo_name, branch_name, token):
         print(
             f"Failed to merge branch '{branch_name}' into main. Response: {response.text}"
         )
+
+
+def merge_multiple_branches_to_main(
+    repo_file_path: Path, branch_name: str, org_name="MonlamAI"
+):
+    """get ids from the txt files (by new lines)"""
+    repo_ids = get_file_content_by_lines(repo_file_path)
+
+    for repo in repo_ids:
+        merge_branch_to_main(f"TM{repo}", branch_name, org_name)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Merging multiple branches to main")
+    parser.add_argument(
+        "file_path",
+        type=Path,
+        help="TM ids to be added",
+    )
+    parser.add_argument(
+        "--branch_name",
+        type=str,
+        help="The branch name to merged with main",
+    )
+    args = parser.parse_args()
+
+    if args.file_path:
+        merge_multiple_branches_to_main(args.file_path, args.branch_name)
+    else:
+        print("Please provide a file path that contains TM ids and branch name")
