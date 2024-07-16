a78120502fc379d11537316ec0cfbe916dd99c1c
Tenzin <tenzintsunduebhattu@gmail.com>
2024-07-16 16:20:42 +0530

    update tokenizer with better speed

--- src/mt_aligner_prep_tool/tokenizers.py
+++ src/mt_aligner_prep_tool/tokenizers.py
@@ -1,26 +1,17 @@
 import re
 from typing import List
 
-import botok
 from spacy.lang.en import English
+from bo_sent_tokenizer import segment
 
 from mt_aligner_prep_tool.utility import SuppressStdout
 
-bo_word_tokenizer = None
 en_nlp = English()
 en_nlp.add_pipe("sentencizer")
 en_nlp.max_length = 5000000
 
 # Types
 SENT_PER_LINE_STR = str  # sentence per line string
-IS_AFFIX_PART = bool
-
-
-def get_bo_word_tokenizer():
-    global bo_word_tokenizer
-    if bo_word_tokenizer is None:
-        bo_word_tokenizer = botok.WordTokenizer()
-    return bo_word_tokenizer
 
 
 def join_sentences(sentences):
@@ -50,69 +41,10 @@ def en_word_tokenizer(text: str) -> List[str]:
     return words
 
 
-def bo_preprocess(text: str) -> str:
-    text = text.replace("\r", "").replace("\n", "")
-    return text
-
 
 def bo_sent_tokenizer(text: str) -> SENT_PER_LINE_STR:
-    """Tokenize a text into sentences."""
-
-    def get_token_text(token):
-        if hasattr(token, "text_cleaned") and token.text_cleaned:
-            return token.text_cleaned
-        else:
-            return token.text
-
-    # fmt: off
-    opening_puncts = ['༁', '༂', '༃', '༄', '༅', '༆', '༇', '༈', '༉', '༊', '༑', '༒', '༺', '༼', '༿', '࿐', '࿑', '࿓', '࿔', '࿙']  # noqa: E501
-    closing_puncts = ['།', '༎', '༏', '༐', '༔', '༴', '༻', '༽', '༾', '࿚']  # noqa: E501
-    skip_chunk_types = [botok.vars.CharMarkers.CJK.name, botok.vars.CharMarkers.LATIN.name]
-    # fmt: on
-
-    # Regex to improve the chunking of shunits, this will be replaced by a better sentence segmentation in botok
-    r_replace = [
-        (r"༼༼[༠-༩]+[བན]༽", r""),  # delete source image numbers `ས་༼༤བ༽མེད་བ`
-        (
-            r"([^ང])་([༔།])",
-            r"\1\2",
-        ),  # delete spurious spaces added by botok in the cleantext values
-        (
-            r"([།གཤ]{1,2})\s+(།{1,2})",
-            r"\1\2 ",
-        ),  # Samdong Rinpoche style double shad. This needs to be applied on inference input
-        # (r"", r""),
-    ]
-
-    text = bo_preprocess(text)
-    sents_words = []
-    current_sentence = []
-    tokenizer = get_bo_word_tokenizer()
-    tokens = tokenizer.tokenize(text, split_affixes=False)
-    for token in tokens:
-        if token.chunk_type in skip_chunk_types:
-            continue
-        token_text = get_token_text(token)
-        if any(punct in token_text for punct in opening_puncts):
-            current_sentence.append(token_text.strip())
-        elif any(punct in token_text for punct in closing_puncts):
-            current_sentence.append(token_text.strip())
-            sents_words.append("".join(current_sentence))
-            current_sentence = []
-        else:
-            current_sentence.append(token_text)
-
-    # If there's any remaining text in the current_sentence
-    if current_sentence:
-        sents_words.append("".join(current_sentence))
-
-    # Join all sentences with a newline
-    sents_text = "\n".join(sents_words)
-
-    # Apply replacements
-    for fr, to in r_replace:
-        sents_text = re.sub(fr, to, sents_text)
-
+    text = remove_emojis(text)
+    sents_text = segment(text)
     return sents_text
 
 
